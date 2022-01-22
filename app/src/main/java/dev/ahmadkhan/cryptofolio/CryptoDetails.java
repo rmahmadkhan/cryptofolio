8b0b459086732c604c2f3bf27af20e14bcc32fb8
Ahmad Khan <bilalzaafar@gmail.com>
2022-01-23 01:42:49 +0500

    Updated imagePath in cryptoList

--- app/src/main/java/dev/ahmadkhan/cryptofolio/CryptoDetails.java
+++ app/src/main/java/dev/ahmadkhan/cryptofolio/CryptoDetails.java
@@ -1,7 +1,6 @@
 package dev.ahmadkhan.cryptofolio;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class CryptoDetails {
@@ -18,8 +17,8 @@ public class CryptoDetails {
     }
 
     public static List<CryptoDetails> cryptoList = Arrays.asList(
-            new CryptoDetails("Bitcoin", "BTC", 36000, ""),
-            new CryptoDetails("Ethereum", "ETH", 4000, ""),
-            new CryptoDetails("Ripple", "XRP", 0.54, "")
+            new CryptoDetails("Bitcoin", "BTC", 36000, "https://cdn4.iconfinder.com/data/icons/crypto-currency-and-coin-2/256/bitoin_btc_coin_crypto-1024.png"),
+            new CryptoDetails("Ethereum", "ETH", 4000, "https://cdn4.iconfinder.com/data/icons/cryptocoins/227/ETH-alt-1024.png"),
+            new CryptoDetails("Ripple", "XRP", 0.54, "https://cdn0.iconfinder.com/data/icons/cryptocurrency-137/128/1_xrp_coin_xrp_xrp_symbol_xrp_sign_currency_xrp_logo_-362-1024.png")
     );
 }
