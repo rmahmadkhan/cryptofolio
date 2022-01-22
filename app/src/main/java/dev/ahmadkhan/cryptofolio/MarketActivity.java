9a8aafd6d86791b288ff8922015132e9fc0aac89
Ahmad Khan <bilalzaafar@gmail.com>
2022-01-23 00:16:23 +0500

    Added recyclerAdapter in MarketActivity

--- app/src/main/java/dev/ahmadkhan/cryptofolio/MarketActivity.java
+++ app/src/main/java/dev/ahmadkhan/cryptofolio/MarketActivity.java
@@ -1,7 +1,6 @@
 package dev.ahmadkhan.cryptofolio;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -9,6 +8,7 @@ import android.os.Bundle;
 public class MarketActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
+    RecyclerAdapter recyclerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +16,8 @@ public class MarketActivity extends AppCompatActivity {
         setContentView(R.layout.activity_market);
 
 
-        recyclerView = findViewById(R.id.crypto_rv);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
-        recyclerView.setLayoutManager(layoutManager);
+        recyclerView = findViewById(R.id.cryptoRecyclerView);
+        recyclerAdapter = new RecyclerAdapter();
+        recyclerView.setAdapter(recyclerAdapter);
     }
 }
