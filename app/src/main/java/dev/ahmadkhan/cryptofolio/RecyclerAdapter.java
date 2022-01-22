3e2c710ee634daed5a021a01c2d748ae02a8fad6
Ahmad Khan <bilalzaafar@gmail.com>
2022-01-23 00:15:56 +0500

    Created RecyclerAdapter

---
+++ app/src/main/java/dev/ahmadkhan/cryptofolio/RecyclerAdapter.java
@@ -0,0 +1,46 @@
+package dev.ahmadkhan.cryptofolio;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.crypto_list_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.name.setText("Bitcoin");
+        holder.symbol.setText("BTC");
+        holder.price.setText("$36000");
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView image;
+        TextView name, symbol, price;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            image = itemView.findViewById(R.id.cyptoImage);
+            name = itemView.findViewById(R.id.cryptoName);
+            symbol = itemView.findViewById(R.id.cryptoSymbol);
+            price = itemView.findViewById(R.id.cyptoPrice);
+        }
+    }
+}
