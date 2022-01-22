2466d36f870554c842f6aa9464f3a604447e2a67
Bilal Zafar <bilalzaafar@gmail.com>
2022-01-22 23:35:21 +0500

    added crypto list item

--- app/src/main/java/dev/ahmadkhan/cryptofolio/MarketActivity.java
+++ app/src/main/java/dev/ahmadkhan/cryptofolio/MarketActivity.java
@@ -1,14 +1,23 @@
 package dev.ahmadkhan.cryptofolio;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MarketActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_market);
+
+
+        recyclerView = findViewById(R.id.crypto_rv);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
     }
 }
