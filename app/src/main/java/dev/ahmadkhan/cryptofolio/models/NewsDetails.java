bfd9aad8dbc2e7f0934ea45e16c1e58d48174917
Ahmad Khan <rmahmadkhan@icloud.com>
2022-01-24 02:43:21 +0500

    Added link in NewsDetails and removed newsList

--- app/src/main/java/dev/ahmadkhan/cryptofolio/models/NewsDetails.java
+++ app/src/main/java/dev/ahmadkhan/cryptofolio/models/NewsDetails.java
@@ -1,22 +1,15 @@
 package dev.ahmadkhan.cryptofolio.models;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class NewsDetails {
     public final String title;
     public final String text;
     public final String imagePath;
+    public final String link;
 
-    public NewsDetails(String title, String text, String imagePath) {
+    public NewsDetails(String title, String text, String imagePath, String link) {
         this.title = title;
         this.text = text;
         this.imagePath = imagePath;
+        this.link = link;
     }
-
-    public static List<NewsDetails> newsList = Arrays.asList(
-            new NewsDetails("News 1", "Text for news 1",  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRP97gbb4ASJYSUkdtSI5GhODCvLoO0QVX7aA&usqp=CAU"),
-            new NewsDetails("News 2", "Text for news 2",  "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/news-background-design-template-db1bc18b8f91b11bb5e1594a5032e1db.jpg?ts=1589089157"),
-            new NewsDetails("News 3", "Text for news 3",  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp6Wydfl6p4mUE7rJ26HhvNU9stLN2EXlzOw&usqp=CAU")
-    );
 }
