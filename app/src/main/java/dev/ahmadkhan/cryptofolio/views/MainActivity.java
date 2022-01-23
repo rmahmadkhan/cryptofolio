33ad7a2e3627b96d4c309cf40f0352ea55af2552
Ahmad Khan <rmahmadkhan@icloud.com>
2022-01-23 21:37:56 +0500

    Added menu and fragments to MainActivity and Updating the view on selection

--- app/src/main/java/dev/ahmadkhan/cryptofolio/views/MainActivity.java
+++ app/src/main/java/dev/ahmadkhan/cryptofolio/views/MainActivity.java
@@ -1,16 +1,44 @@
 package dev.ahmadkhan.cryptofolio.views;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.annotation.NonNull;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import dev.ahmadkhan.cryptofolio.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.market);
+
+    }
+    MarketFragment marketFragment = new MarketFragment();
+    NewsFragment newsFragment = new NewsFragment();
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.market:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, marketFragment).commit();
+                return true;
+
+            case R.id.news:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, newsFragment).commit();
+                return true;
+        }
+        return false;
     }
 }
+
